741dcc0f6b8de32fe0ba9e34c55be84f8b57adb4
mzahiir <edhorezz021@gmail.com>
2018-05-02 00:03:35 -0700

    Menggunakan Method Set

--- src/praktek03/MobilAksi.java
+++ src/praktek03/MobilAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class MobilAksi {
     public static void main(String[] args) {
         Mobil m = new Mobil();
-        m.merk="Avanza";
-        m.warna="Merah";
-        m.harga=6500000;
+
+        m.setMerk("Ferari");
+        m.setWarna("Hitam");
+        m.setHarga(6500000);
         
         m.cetakInfo();
     }
