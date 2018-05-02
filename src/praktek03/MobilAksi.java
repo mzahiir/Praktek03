a4752691dbe1cc27a1f8de944fcd56e1652a2d6c
mzahiir <edhorezz021@gmail.com>
2018-05-01 23:55:45 -0700

    Instance, Buat Attribute dan Panggil Method

--- src/praktek03/MobilAksi.java
+++ src/praktek03/MobilAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class MobilAksi {
     public static void main(String[] args) {
+        Mobil m = new Mobil();
+        m.merk="Avanza";
+        m.warna="Merah";
+        m.harga=6500000;
         
+        m.cetakInfo();
     }
 }
