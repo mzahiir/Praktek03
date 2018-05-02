120097ceff55a5ed13fd463f420e0f9c830c10f5
mzahiir <edhorezz021@gmail.com>
2018-05-02 00:12:27 -0700

    Attribute da Method

--- src/praktek03/MobilBalap.java
+++ src/praktek03/MobilBalap.java
@@ -1,5 +1,9 @@
 package praktek03;
 
 public class MobilBalap extends Mobil {
+    private String jenisMobil;
     
+    void throttle(){
+        System.out.println("Brrruuuumm..... zzz... zzz...");
+    }
 }
